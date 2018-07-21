81e69cd5f47081ab651ee4f982ef2249a32bd8b9
Ubuntu <ubuntu@master.virt.seld.rnd.ericsson.se>
2018-07-21 22:53:33 +0000

    Updated to match new project structure

--- gym_radio_scheduler/__init__.py
+++ gym_radio_scheduler/__init__.py
@@ -1,4 +1,4 @@
 from gym.envs.registration import register
 
-register(id = 'radio-scheduler-v0',
+register(id = 'RadioScheduler-v0',
          entry_point = 'gym_radio_scheduler.envs:RadioSchedulerEnv')
--- gym_radio_scheduler/envs/radio_scheduler_env.py
+++ gym_radio_scheduler/envs/radio_scheduler_env.py
@@ -2,14 +2,19 @@ import gym
 from gym import error, spaces, utils
 from gym.utils import seeding
 
+from radio_multilink_scheduler import RadioMultilinkScheduler
+
 class RadioSchedulerEnv(gym.Env):
-  metadata = {'render.modes': ['test']}
+  metadata = {'render.modes': ['random']}
   
   def __init__(self):
-    pass
+    self.sched = RadioMultilinkScheduler(nrof_ues=30, scheduler_type='Random')
+
   def step(self, action):
-    pass
+    reward = self.sched.transmit(action)
+
   def reset(self):
     pass
-  def render(self, mode='test', close=False):
+
+  def render(self, mode='random', close=False):
     pass
