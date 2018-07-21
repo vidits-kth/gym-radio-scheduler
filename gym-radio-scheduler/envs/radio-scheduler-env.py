9d6118dd6791799b78413b43ea8f3e14a1998556
Vidit Saxena <vidits@kth.se>
2018-07-20 22:50:49 -0700

    Update radio-scheduler-env.py

--- gym-radio-scheduler/envs/radio-scheduler-env.py
+++ gym-radio-scheduler/envs/radio-scheduler-env.py
@@ -1,1 +1,15 @@
+import gym
+from gym import error, spaces, utils
+from gym.utils import seeding
 
+class RadioSchedulerEnv(gym.Env):
+  metadata = {'render.modes': ['test']}
+  
+  def __init__(self):
+    pass
+  def step(self, action):
+    pass
+  def reset(self):
+    pass
+  def render(self, mode='test', close=False):
+    pass
