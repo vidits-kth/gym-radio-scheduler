69a26c448cf4ca1358423585dbf86e9a373026cc
Ubuntu <ubuntu@master.virt.seld.rnd.ericsson.se>
2018-07-23 20:37:27 +0000

    Documentation and action handling

--- gym_radio_scheduler/envs/radio_scheduler_env.py
+++ gym_radio_scheduler/envs/radio_scheduler_env.py
@@ -5,21 +5,47 @@ from gym.utils import seeding
 from .radio_multilink_scheduler import RadioMultilinkScheduler
 
 class RadioSchedulerEnv(gym.Env):
-  metadata = {'render.modes': ['random']}
+#    metadata = {'render.modes': ['random']}
+    
+    def __init__(self):
   
-  def __init__(self):
-
-    # nrof_ues: Integer value
-    # scheduler_type: ['Random', 'MaxRate', 'RoundRobin', 'PropFair']
-    self.sched = RadioMultilinkScheduler(nrof_ues=30, scheduler_type='Random')
-
-  def step(self, action=-1):
-    result = self.sched.transmit()
-
-    return result
-
-  def reset(self):
-    pass
-
-  def render(self, mode='random', close=False):
-    pass
+        # nrof_ues: Integer value
+        # scheduler_type: ['Random', 'MaxRate', 'RoundRobin', 'PropFair']
+        self.sched = RadioMultilinkScheduler(nrof_ues=30, scheduler_type='Random')
+  
+    def step(self, action=-1):
+        """
+           A step in the environment.
+     
+           Parameters
+           ----------
+           action : int in range [0, 30]
+           
+           action: 
+               The UE specified by the action is scheduled in the next
+               transmission time interval (TTI). If action is -1, the traditional
+               radio scheduler schedules one of the UEs with index [0,...,30].
+     
+           Returns
+           -------
+           scheduled_ue_index, channel_quality_index, realized_throughput : tuple
+     
+           scheduled_ue_index (int) :
+               UE scheduled in the current TTI either by specified action or
+               by the radio scheduler
+           channel_quality_index (int) :
+               The observed channel quality index (CQI) for the UE scheduled
+               in the current TTI.
+           realized_throughput (int) :
+               Throughput is 0 if tranmsission failed or UE was out of range,
+               otherwise the number of information bits transmitted successfully.
+        """
+        result = self.sched.transmit(action)
+  
+        return result
+  
+    def reset(self):
+        pass
+  
+    def render(self, mode='random', close=False):
+        pass
